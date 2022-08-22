1606b39f7241d29050a421da54f9f373d2624ddb
mbeltran <mariacecilbeltran@gmail.com>
2022-08-21 21:06:54 -0300

    primer desafio complementario js

--- pag.js
+++ pag.js
@@ -1,26 +1,8 @@
-var remera;
-var campera;
-var color;
-var talle;
-remera = confirm ("Quieres llevar una remera?");
-color = prompt ("de que color prefieres?");
-campera = confirm ("Quieres llevar una campera?");
-talle = Number (prompt ("Cual es tu talle?") );
-
-if (remera === true && color === "negro") {
-    console.log ("que buena elección!!");
-}else if (campera === true && talle <= 35){
-    console.log ("no contamos con la mercadería solicitada");
-}
-else {
-    console.log ("Lamentamos no contar con lo solicitado 😔");
-}
-
 
 //FOR//
 
 let i=1
-for (i=1; i<30; i++){
+for (i=1; i<10; i++){
     let nombre = prompt ("Ingrese su nombre");
     alert ("Bienvenido! " + nombre + " se ha inscripto en el lugar n° "+ i);
     console.log ("Se ha inscripto el estudiante con el nombre de  " + nombre + "  en el lugar n° " + i );
@@ -30,17 +12,23 @@ for (i=1; i<30; i++){
 
 let contraseña = Number (prompt ("Ingrese su contraseña"));
 let counter = 0;
-while (contraseña !== 122 && counter < 3) {
+while (contraseña !== 33 && counter < 3) {
     alert ("Contraseña incorrecta, vuelva a ingresarla");
     console.log ("Contraseña incorrecta");
     let contraseña = Number (prompt ("Ingrese su contraseña"));
     counter++ 
 }
-
 if (counter <2) {
     console.log ("ingreso correcto");   
 } else {
   console.log ("no hay mas intentos")
 }
 
+
+let problema;
+do { 
+  console.log ("1- no recuerdas tu contraseña \n 2-no estás registrado \n 0 - salir");
+  problema = Number (prompt("elija una opción"));
+}while (problema !== 0 );
+  
 
