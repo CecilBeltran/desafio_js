3907daeb93b1f419ba0ee668efbfbec8ef1f9bf2
mbeltran <mariacecilbeltran@gmail.com>
2022-09-22 18:43:27 -0300

    segunda entrega proyecto final

--- js/pag.js
+++ js/pag.js
@@ -1,45 +1,46 @@
-let arrayReserva= [];
-let arrayReservaSala=[];
+let arrayReserva = [];
+let arrayReservaSala = [];
 
 class entrenamientoFuncional {
-  constructor(id,day,hour,reserved) {
-    this.id= id;
-    this.day= day;
-    this.hour= hour;
-    this.reserved= reserved;
-} };
+  constructor(id, day, hour, reserved) {
+    this.id = id;
+    this.day = day;
+    this.hour = hour;
+    this.reserved = reserved;
+  }
+};
 
-const funcional1= new entrenamientoFuncional (10, "Lunes","8:00", false);
-const funcional2= new entrenamientoFuncional (20, "Martes", "8:00", false);
-const funcional3= new entrenamientoFuncional (30, "Miércoles", "8:00", false);
-const funcional4= new entrenamientoFuncional (40, "Jueves", "8:00", false);
-const funcional5= new entrenamientoFuncional (50, "Viernes","8:00", false);
+const funcional1 = new entrenamientoFuncional(10, "Lunes", "8:00", false);
+const funcional2 = new entrenamientoFuncional(20, "Martes", "8:00", false);
+const funcional3 = new entrenamientoFuncional(30, "Miércoles", "8:00", false);
+const funcional4 = new entrenamientoFuncional(40, "Jueves", "8:00", false);
+const funcional5 = new entrenamientoFuncional(50, "Viernes", "8:00", false);
 
 
-let actividadesFuncional=[ ];
+let actividadesFuncional = [];
 actividadesFuncional.push(funcional1);
 actividadesFuncional.push(funcional2);
 actividadesFuncional.push(funcional3);
 actividadesFuncional.push(funcional4);
-actividadesFuncional.push (funcional5);
+actividadesFuncional.push(funcional5);
 
 
 
-function salaEntrenamiento (id,day,hour,reserved) {    
-    this.id= id;
-    this.day= day;
-    this.hour= hour;
-    this.reserved= reserved;
+function salaEntrenamiento(id, day, hour, reserved) {
+  this.id = id;
+  this.day = day;
+  this.hour = hour;
+  this.reserved = reserved;
 };
 
-const sala1= new salaEntrenamiento (60, "Lunes","8:00", false);
-const sala2= new salaEntrenamiento (70, "Martes","8:00", false);
-const sala3= new salaEntrenamiento (80, "Miercoles","8:00", false);
-const sala4= new salaEntrenamiento (90, "Jueves","8:00", false);
-const sala5= new salaEntrenamiento (100, "Viernes","8:00", false);
+const sala1 = new salaEntrenamiento(60, "Lunes", "8:00", false);
+const sala2 = new salaEntrenamiento(70, "Martes", "8:00", false);
+const sala3 = new salaEntrenamiento(80, "Miercoles", "8:00", false);
+const sala4 = new salaEntrenamiento(90, "Jueves", "8:00", false);
+const sala5 = new salaEntrenamiento(100, "Viernes", "8:00", false);
 
 
-let activdadesSala =[];
+let activdadesSala = [];
 
 activdadesSala.push(sala1);
 activdadesSala.push(sala2);
@@ -51,96 +52,211 @@ activdadesSala.push(sala5);
 //IMPRIMIR CARTAS
 function mostrarActividades(entrenamientoFuncional) {
   const contenedorDeActividades = document.getElementById("contenedor");
-  
+
   actividadesFuncional.forEach(entrenamientoFuncional => {
-  console.log(entrenamientoFuncional);
-  
-  const divActividad = document.createElement("div");
-  divActividad.classList.add ("actividad");
-  divActividad.innerHTML=  `  <p> ${entrenamientoFuncional.day} Hora ${entrenamientoFuncional.hour}</p>`;
-  contenedorDeActividades.appendChild(divActividad);
-  console.log(entrenamientoFuncional);
-  
-  const botonVerDetalle = crearBotonVerDetalle(entrenamientoFuncional)
-  divActividad.appendChild(botonVerDetalle);
-  contenedorDeActividades.appendChild(divActividad);
-}
-)
+
+
+    const divActividad = document.createElement("div");
+    divActividad.classList.add("actividad");
+    divActividad.innerHTML = `  <p> ${entrenamientoFuncional.day} Hora ${entrenamientoFuncional.hour}</p>`;
+    contenedorDeActividades.appendChild(divActividad);
+    
+
+    const botonVerDetalle = crearBotonVerDetalle(entrenamientoFuncional)
+    divActividad.appendChild(botonVerDetalle);
+    contenedorDeActividades.appendChild(divActividad);
+  }
+  )
 };
 
-function mostrarDetalle(entrenamientoFuncional){
-  arrayReserva.push(entrenamientoFuncional)
-  console.log(arrayReserva);
-  localStorage.setItem("reserva", JSON.stringify(entrenamientoFuncional));
-  entrenamientoFuncional.length >0 ? entrenamientoFuncional: entrenamientoFuncional = JSON.parse(localStorage.getItem("reserva"))
-  console.log(entrenamientoFuncional);
+function mostrarDetalle(arrayReserva) {
+
+
+  arrayReserva ? arrayReserva : arrayReserva = JSON.parse(localStorage.getItem("reserva"))  ///// RECUPERO SI HAY COSAS GUARDADOS EN EL LOCALSTORAGE
+
+
+
   const contenedorDeActividadFuncional = document.getElementById("contenedor-funcional");
-  var item;
-  arrayReserva.map(entrenamientoFuncional=> item= `
-  <h3>Entrenamiento Funcional</h3>
-  <p> ${entrenamientoFuncional.day}</p>
-  <p>${entrenamientoFuncional.hour}</p> `)
-  console.log(item);
-  contenedorDeActividadFuncional.innerHTML = item ;
+
+  console.log(arrayReserva);
+
+
+  var item = ``;
+
+  arrayReserva.map(entrenamientoFuncional => [item += /////////////// SUMO TODOS LOS ITEMS ITERADOS 
+    `
   
+  <p> ${entrenamientoFuncional.day}</p>
+  <p>${entrenamientoFuncional.hour}</p> `])
+
+
+
+  contenedorDeActividadFuncional.innerHTML = "<h3>Entrenamiento Funcional</h3>" + item; ////// un solo titulo y sumado todos los deas
+
 }
 
-function crearBotonVerDetalle(entrenamientoFuncional){
-  console.log(entrenamientoFuncional);
+////////////////////////////////////// ELIMINAR REPETIDOS
+function eliminarRepetidos(params) {
+
+  const unicoFav = new Set(fav);
+  clearFav = [...unicoFav]
+
+  return fav
+}
+
+/////////////////////////////////////
+
+
+let fav = JSON.parse(localStorage.getItem("reserva")) || [] /////// ME FIJO SI HAY ALGO GUARDADO, SINO LO CARGO COMO ARRAY VACIO
+
+let clearFav;
+///////////////// GUARDADO LOCAL STORAGE
+function guardadoLocalStorage(entrenamientoFuncional) {
+
+
+  fav.push(entrenamientoFuncional)///////// PUSHE LOS OBJETOS EN LA RESERVA EN UN ARRAY VACIO
+  fav = eliminarRepetidos(fav)
+
+
+  localStorage.setItem("reserva", JSON.stringify(fav));////// GUARDO EN EL LOCALSTORAGE
+  return clearFav
+}
+
+
+////////////////////////////////////////
+
+
+
+function crearBotonVerDetalle(entrenamientoFuncional) {
+
+
   const button = document.createElement("button");
   button.innerText = "Reservar";
-  button.addEventListener("click", () => {  button.innerHTML=`reservado`
-  mostrarDetalle(entrenamientoFuncional);
+  button.addEventListener("click", () => {
+    button.innerHTML = `reservado`
+
+
+
+
+    arrayReserva = guardadoLocalStorage(entrenamientoFuncional)
+
+
+
+
+
+    mostrarDetalle(arrayReserva);
   }
   )
   return button;
 }
 
-
 mostrarActividades(entrenamientoFuncional)
 
-//MOSTRAR ACTIVIDADES DE SALA ENTRENAMIENTO Y EVENTO BOTONES
+
+
+
+// //MOSTRAR ACTIVIDADES DE SALA ENTRENAMIENTO Y EVENTO BOTONES
+
+
 function mostrarActividadesSala(salaEntrenamiento) {
-  const contenedorDeActividades1 = document.getElementById("contenedor1");
+  const contenedorDeActividadesSala = document.getElementById("contenedor1");
+
   activdadesSala.forEach(salaEntrenamiento => {
-  console.log(salaEntrenamiento);
-  const divActividadSala = document.createElement("div");
-  divActividadSala.classList.add ("actividadSala");
-  divActividadSala.innerHTML=  `  <p> ${salaEntrenamiento.day} Hora ${salaEntrenamiento.hour}</p>`;
-  contenedorDeActividades1.appendChild(divActividadSala);
-  const botonVer = crearBotonVerSala(salaEntrenamiento)
-  divActividadSala.appendChild(botonVer);
-  contenedorDeActividades1.appendChild(divActividadSala);
+
+
+    const divActividadSala = document.createElement("div");
+    divActividadSala.classList.add("actividadSala");
+    divActividadSala.innerHTML = `  <p> ${salaEntrenamiento.day} Hora ${salaEntrenamiento.hour}</p>`;
+    contenedorDeActividadesSala.appendChild(divActividadSala);
+ 
+
+    const botonVerDetalleSala = crearBotonVerDetalleSala(salaEntrenamiento)
+    divActividadSala.appendChild(botonVerDetalleSala);
+    contenedorDeActividadesSala.appendChild(divActividadSala);
   }
   )
 };
-function crearBotonVerSala(salaEntrenamiento){
+
+function mostrarDetalleSala(arrayReservaSala) {
+
+
+  arrayReservaSala ? arrayReservaSala : arrayReservaSala = JSON.parse(localStorage.getItem("reservaSala"))  ///// RECUPERO SI HAY COSAS GUARDADOS EN EL LOCALSTORAGE
+
+
+
+  const contenedorDeActividadesSala = document.getElementById("contenedor-sala");
+
+  console.log(arrayReservaSala);
+
+
+  var itemS = ``;
+
+  arrayReservaSala.map(salaEntrenamiento => [itemS += /////////////// SUMO TODOS LOS ITEMS ITERADOS 
+    `
+  
+  <p> ${salaEntrenamiento.day}</p>
+  <p>${salaEntrenamiento.hour}</p> `])
+
+
+
+  contenedorDeActividadesSala.innerHTML = "<h3>Sala de Entrenamiento</h3>" + itemS; ////// un solo titulo y sumado todos los deas
+
+}
+
+////////////////////////////////////// ELIMINAR REPETIDOS 
+function eliminarRepetidosSala(params) {
+
+  const unicoFavS = new Set(favS);
+  clearFavS = [...unicoFavS]
+
+  return favS
+}
+
+/////////////////////////////////////
+
+
+let favS = JSON.parse(localStorage.getItem("reservaSala")) || [] /////// ME FIJO SI HAY ALGO GUARDADO, SINO LO CARGO COMO ARRAY VACIO
+
+let clearFavS;
+///////////////// GUARDADO LOCAL STORAGE
+function guardadoLocalStorageSala(salaEntrenamiento) {
+
+
+  favS.push(salaEntrenamiento)///////// PUSHE LOS OBJETOS EN LA RESERVA EN UN ARRAY VACIO
+  favS = eliminarRepetidosSala(favS)
+
+
+  localStorage.setItem("reservaSala", JSON.stringify(favS));////// GUARDO EN EL LOCALSTORAGE
+  return clearFavS
+}
+
+
+////////////////////////////////////////7
+
+
+
+function crearBotonVerDetalleSala(salaEntrenamiento) {
+
+
   const button2 = document.createElement("button");
   button2.innerText = "Reservar";
-  button2.addEventListener("click", () => {  button2.innerHTML=`reservado`
-    mostrarDetalleSala(salaEntrenamiento);
-    })
+  button2.addEventListener("click", () => {
+    button2.innerHTML = `reservado`
+
+
+    arrayReservaSala = guardadoLocalStorageSala(salaEntrenamiento)
+
+    mostrarDetalleSala(arrayReservaSala);
+  }
+  )
   return button2;
 }
-function mostrarDetalleSala(salaEntrenamiento){
-  arrayReservaSala.push(salaEntrenamiento)
-  console.log(arrayReservaSala);
-  localStorage.setItem("reservaSala", JSON.stringify(salaEntrenamiento));
-  salaEntrenamiento.length >0 ? salaEntrenamiento: salaEntrenamiento = JSON.parse(localStorage.getItem("reservaSala"))
-  const contenedorDeActividadSala = document.getElementById("contenedor-sala");
-  var item;
-  arrayReserva.map(salaEntrenamiento=> item= `
-  <h3>Entrenamiento Funcional</h3>
-  <p> ${salaEntrenamiento.day}</p>
-  <p>${salaEntrenamiento.hour}</p> `)
-  console.log(item);
-  contenedorDeActividadSala.innerHTML = item ;
-  
 
-    
-}mostrarActividadesSala(salaEntrenamiento)
+
+mostrarActividadesSala(salaEntrenamiento)
+
+mostrarDetalle(); 
+mostrarDetalleSala();
 
 
 
-
- 
